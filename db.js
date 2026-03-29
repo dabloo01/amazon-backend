65c4890af2fe8085c258de610d99054dcae2a375
Dabloo <dablookr8434@gmail.comn>
2026-03-29 15:38:39 +0530

    my changes

--- db.js
+++ db.js
@@ -11,8 +11,4 @@ export const pool = new Pool({
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT,
-  ssl:
-    process.env.NODE_ENV === "production"
-      ? { rejectUnauthorized: false }
-      : false,
 });
